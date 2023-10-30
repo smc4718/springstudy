c0547a4bac622656f18c40014e2fc29aa478e3d7
smc4718 <smc4718@naver.com>
2023-10-30 15:54:51 +0900

    231030(4)

--- workspace-sts-3.9.18.RELEASE/15_myhome/src/main/java/com/gdu/myhome/controller/BlogController.java
+++ workspace-sts-3.9.18.RELEASE/15_myhome/src/main/java/com/gdu/myhome/controller/BlogController.java
@@ -77,7 +77,11 @@ public class BlogController {
     return blogService.addComment(request);
   }
   
-  
+  @ResponseBody
+  @GetMapping(value="/commentList.do", produces="application/json")
+  public Map<String, Object> commentList(HttpServletRequest request){
+    return blogService.loadCommentList(request);
+  }
   
   
   
